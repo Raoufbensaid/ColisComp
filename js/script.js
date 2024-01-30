b4c9525bafd58d61a57336ede2111c939106aca6
unknown <RaoufBensaid>
2024-01-30 14:36:52 +0100

    update menu

--- js/script.js
+++ js/script.js
@@ -9,29 +9,3 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const decreaseBtn = document.getElementById('decreaseBtn');
-    const increaseBtn = document.getElementById('increaseBtn');
-    const counterValue = document.getElementById('counterValue');
-
-    let count = 0;
-
-    function updateCounter() {
-        counterValue.value = count;
-    }
-
-    decreaseBtn.addEventListener('click', function () {
-        // Vérifier que la valeur de count est supérieure à zéro avant de décrémenter
-        if (count > 0) {
-            count--;
-            updateCounter();
-        }
-    });
-
-    increaseBtn.addEventListener('click', function () {
-        // Incrémenter directement, car il n'y a pas de restriction sur les nombres positifs
-        count++;
-        updateCounter();
-    });
-});
